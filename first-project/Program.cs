7984b4d5468630bff34098c59c2aacbede2c58bb
g0dl3ss1337 <g0dl3ss1337@gmail.com>
2023-09-04 17:14:55 +0300

    first commit

---
+++ first-project/Program.cs
@@ -0,0 +1,12 @@
+﻿sing System;
+
+namespace first_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
